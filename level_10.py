52b7104fe5f812ba3f392cbb22f5014d7ec6d710
shankaresh <33858439+shankaresh@users.noreply.github.com>
2019-08-07 22:25:57 +0530

    Create level_10.py

---
+++ level_10.py
@@ -0,0 +1,18 @@
+# Cut and paste the JavaScript code and assign it 
+# to the variable below 
+
+level_10_code = "while(notDone()){moveForward();if(isPathForward()){if(isPathRight()){turnRight();}else{if(isPathLeft()){turnLeft();}}}else{if (isPathLeft()){turnLeft();}else{turnRight();}}}"
+
+
+
+
+# Transfer the value from Level 10 "blocks remaining"
+# to the variable below 
+
+remaining_blocks = 0 
+
+
+print("JavaScript code for Level 10")
+print(level_10_code)
+
+print("Number of blocks remaining", remaining_blocks)
