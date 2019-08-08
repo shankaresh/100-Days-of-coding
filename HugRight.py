3e99a941720c88378685501620750de3632a2136
shankaresh <33858439+shankaresh@users.noreply.github.com>
2019-08-08 22:21:24 +0530

    Create HugRight.py

---
+++ HugRight.py
@@ -0,0 +1,23 @@
+# Cut and paste the JavaScript code and assign it 
+# to the variable below 
+
+level_10_code = """while (notDone()) {
+  if (isPathRight()) {
+    turnRight();
+  }
+  if (isPathForward()) {
+    moveForward();
+  } else {
+    turnLeft();
+  }
+}"""
+
+# Transfer the value from Level 10 "blocks remaining"
+# to the variable below 
+
+remaining_blocks = 4
+
+print("JavaScript code for Level 10")
+print(level_10_code)
+
+print("Number of blocks remaining", remaining_blocks)
