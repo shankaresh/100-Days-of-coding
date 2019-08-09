69ea9f5dea3d26287bfa92526942f87dca73a18b
shankaresh <33858439+shankaresh@users.noreply.github.com>
2019-08-09 13:37:46 +0530

    Update AreaOfRightTriangle.py

--- AreaOfRightTriangle.py
+++ AreaOfRightTriangle.py
@@ -1,3 +1,17 @@
+## Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a separate line.
+
+
+
+```
+Example input
+3
+5
+
+Example output
+7.5
+```
+
+~~~
 # Read the numbers b and h like this:
 b = int(input())
 h = int(input())
@@ -5,3 +19,4 @@ h = int(input())
 # Print the result with print()
 A = 0.5*b*h 
 print(A)
+~~~
